7cf81c6f7f91f3199f8c9b4b159ececca094309e
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-03-06 19:09:08 +0630

    update (Test1.java) : add new lines

--- src/Test1.java
+++ src/Test1.java
@@ -6,5 +6,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello Main 1");
         System.out.println("Hello Main 2");
+        System.out.println("Hello Dev 3");
     }
 }
