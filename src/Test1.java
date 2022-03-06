f607ad18aaa8d7ab71cf3f07c59a9f60483f3458
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-03-06 19:05:37 +0630

    init (Test1.java) : create Test1.java file

---
+++ src/Test1.java
@@ -0,0 +1,10 @@
+/**
+ * @author Hein Htet Aung
+ * @created at 06/03/2022
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Main 1");
+        System.out.println("Hello Main 2");
+    }
+}
