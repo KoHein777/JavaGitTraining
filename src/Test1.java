0db55dccc69c2d011d045515eaa97b2a9425e281
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-04-12 23:32:03 +0630

    update (Test1.java) : add new lines

--- src/Test1.java
+++ src/Test1.java
@@ -4,10 +4,6 @@
  **/
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello Main 0");
-        System.out.println("Hello Main 1");
-        System.out.println("Hello Main 2");
-        System.out.println("Hello Dev 3");
-        System.out.println("Hello Main 3");
+        System.out.println("I am learning devops");
     }
 }
