2cc3104e25b0fbda00b9c5738ac5aa6d927d5151
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-04-14 01:15:10 +0630

    modification (Test1.java) : added new line

--- src/Test1.java
+++ src/Test1.java
@@ -4,6 +4,8 @@
  **/
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("I am learning devops training");
+        System.out.println("I am web developer");
+        System.out.println("I am learning devops");
+        System.out.println("I am java developer ");
     }
 }
