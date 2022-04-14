9c6241c150fa6c700dd8f4f5a3923fd0ec70a4e6
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-04-14 17:36:12 +0630

    modification (Test3.java) : changed word

---
+++ src/Test3.java
@@ -0,0 +1,9 @@
+/**
+ * @author Hein Htet Aung
+ * @created at 14/04/2022
+ **/
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
