7887cf9095b8facd9b86a6b9ef1e34a361af991e
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-04-14 17:35:32 +0630

    modification (Test2.java) : changed word

--- src/Test2.java
+++ src/Test2.java
@@ -5,7 +5,6 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Learning Devops Training");
-        System.out.println("Hello Training");
-        System.out.println("I am learning");
+
     }
 }
