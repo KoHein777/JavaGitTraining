1910e9e9d83e26f1d2206e4ca09eb142974af849
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-04-14 16:40:30 +0630

    init (Test2.java) : create Test2 java file

---
+++ src/Test2.java
@@ -0,0 +1,9 @@
+/**
+ * @author Hein Htet Aung
+ * @created at 14/04/2022
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Test2");
+    }
+}
