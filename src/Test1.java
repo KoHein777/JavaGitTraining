f04bcf0deb3eda5414c93ebe69fe656a19a321dd
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-04-14 22:21:15 +0630

    modification (Test1.java) : changed word

--- src/Test1.java
+++ src/Test1.java
@@ -4,6 +4,6 @@
  **/
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("I am web developer");
+        System.out.println("I am web developer using java");
     }
 }
