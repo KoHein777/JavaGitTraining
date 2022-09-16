b0a6f2ab916a4b6689a027a81db1b495cb8e397c
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-09-16 15:33:13 +0630

    modification (Test1.java) :   - remove

--- src/Test1.java
+++ src/Test1.java
@@ -6,6 +6,5 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Learning Git Stash");
         System.out.println("Hello World");
-        System.out.println("12345678");
     }
 }
