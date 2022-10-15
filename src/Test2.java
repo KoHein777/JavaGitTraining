31b3ccfc50f859c844c4acb0846b8654ab4597f9
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-10-15 16:04:33 +0630

    Revert "modification (Test2.java) :"
    This reverts commit 3456c8ee8d31a10a3e176bcf7292338703b5964f.

--- src/Test2.java
+++ src/Test2.java
@@ -5,7 +5,6 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Learning Devops Training");
-        System.out.println("This is line 1");
 
     }
 }
