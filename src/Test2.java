3456c8ee8d31a10a3e176bcf7292338703b5964f
HeinHtetAung <heinhtetaungcu@gmail.com>
2022-10-15 16:02:19 +0630

    modification (Test2.java) :   - add   line 1

--- src/Test2.java
+++ src/Test2.java
@@ -5,6 +5,7 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Learning Devops Training");
+        System.out.println("This is line 1");
 
     }
 }
